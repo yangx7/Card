5b60bcffe91801a24690236649fb166149ba9344
Yangx7 <yxiao12@uci.edu>
2021-09-03 23:19:28 -0700

    add react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/home";
 import "./styles/index.scss";
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
