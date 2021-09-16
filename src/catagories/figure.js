c12326b95fbb878f8e2d8872abcc38ada2f4b887
Yangx7 <yxiao12@uci.edu>
2021-09-15 22:44:01 -0700

    fix img relative path

--- src/catagories/figure.js
+++ src/catagories/figure.js
@@ -6,7 +6,11 @@ function Figure() {
   return (
     <div className="figure">
       <div className="card__face card__face__front">
-        <img src={images["C.Ronaldo.jpg"]} alt="" className="img" />
+        <img
+          src={`${window.location.origin}/${images["C.Ronaldo.jpg"]}`}
+          alt=""
+          className="img"
+        />
       </div>
       <div className="card__face card__face__back">
         <h2>C.Ronaldo</h2>
